8ffc3914321db8d2412d4d9d9d1830fecf4a1865
Presbury <wamaepresbury034@gmail.com>
2025-02-27 18:02:46 +0300

    creates index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const mainElement = document.querySelector("main#main");
+if (mainElement) {
+  // Remove the element from the DOM
+  mainElement.remove();
+}
+const newHeader = document.createElement("h1");
+newHeader.textContent = "YOUR NAME is the champion";
+document.body.appendChild(newHeader);
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "YOUR NAME is the champion";
+document.body.appendChild(newHeader);
